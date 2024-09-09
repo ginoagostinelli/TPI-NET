89065d94cf49051ac1313761453155bfedce1a52
lautaronahuelp <lautaro@outlook.com>
2024-09-09 20:54:36 -0300

    Se crea SolicitudService

--- Dominio/Services/SolicitudService.cs
+++ Dominio/Services/SolicitudService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dominio.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace Dominio.Services
 {
     public class SolicitudService
     {
+        public void Add(Solicitud solicitud)
+        {
+            using var context = new EmpresaContext();
+
+            context.Solicitudes.Add(solicitud);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new EmpresaContext();
+
+            Solicitud? solicitudToDelete = context.Solicitudes.Find(id);
+
+            if (solicitudToDelete != null)
+            {
+                context.Solicitudes.Remove(solitudToDelete);
+                context.SaveChanges();
+            }
+        }
     }
 }
