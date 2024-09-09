53462f6e64b71f4232af857ee1b6ef014ff9b4ba
lautaronahuelp <lautaro@outlook.com>
2024-09-09 20:41:41 -0300

    Se crea EmpresaContext

--- Dominio/EmpresaContext.cs
+++ Dominio/EmpresaContext.cs
@@ -1,7 +1,26 @@
-﻿namespace Dominio
-{
-    public class EmpresaContext
-    {
+﻿using Dominio.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.Entity;
 
+namespace Dominio
+{
+    internal class EmpresaContext : DbContext
+    {
+        internal DbSet<Cliente> Clientes { get; set; }
+        internal DbSet<Tecnico> Tecnicos { get; set; }
+        internal DbSet<Solicitud> Solicitudes { get; set; }
+        internal DbSet<Visita> Visitas { get; set; }
+        internal DbSet<Material> Materiales { get; set; }
+        internal DbSet<TipoSolicitud> TiposSolicitudes { get; set; }
+        internal DbSet<TipoMaterial> TiposMateriales { get; set; }
+
+        internal EmpresaContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=ClienteDb");
     }
 }
