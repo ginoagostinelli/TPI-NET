5513b0e78fb2b70a3eed6dcca18fe74aff59b803
ginoagostinelli <ginoagostinelli5@gmail.com>
2024-09-09 20:24:00 -0300

    dominio y form inicial

---
+++ Dominio/Model/Solicitud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Clases
+{
+    internal class Solicitud
+    {
+        public int Id { get; set; }
+        public string Motivo {  get; set; }
+
+        
+    }
+}
---
+++ Dominio/Model/Visita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Model
+{
+    public class Visita
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public bool DebeVolver { get; set; }
+
+        public DateOnly Fecha { get; set; }
+    }
+}
