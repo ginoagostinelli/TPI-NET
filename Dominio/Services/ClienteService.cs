b333f0d1f5654bfa01c8d658050af4acdb432a45
ginoagostinelli <ginoagostinelli5@gmail.com>
2024-09-09 21:11:51 -0300

    cliente service

---
+++ Dominio/Services/ClienteService.cs
@@ -0,0 +1,57 @@
+﻿using Dominio;
+using Dominio.Model;
+
+namespace Domain.Services
+{
+    public class ClienteService
+    {
+        public void Add(Cliente cliente)
+        {
+            using var context = new EmpresaContext();
+
+            context.Clientes.Add(cliente);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new EmpresaContext();
+
+            Cliente? clienteToDelete = context.Clientes.Find(id);
+
+            if (clienteToDelete != null)
+            {
+                context.Clientes.Remove(clienteToDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public Cliente? Get(int id)
+        {
+            using var context = new EmpresaContext();
+
+            return context.Clientes.Find(id);
+        }
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            using var context = new EmpresaContext();
+
+            return context.Clientes.ToList();
+        }
+
+        public void Update(Cliente cliente)
+        {
+            using var context = new EmpresaContext();
+
+            Cliente? clienteToUpdate = context.Clientes.Find(cliente.Id);
+
+            if (clienteToUpdate != null)
+            {
+                clienteToUpdate.Nombre = cliente.Nombre;
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
