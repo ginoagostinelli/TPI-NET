eab35bfd562b7fc088d4f9464b290c4973567517
ginoagostinelli <ginoagostinelli5@gmail.com>
2024-09-09 21:09:34 -0300

    agrega cliente al dominio

--- Dominio/Model/Cliente.cs
+++ Dominio/Model/Cliente.cs
@@ -8,5 +8,14 @@ namespace Dominio.Model
 {
     public class Cliente
     {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Razon_Social { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public string Conclusion { get; set; }
     }
 }
